d9e875788f04db0cbaf214d771ba86cc0dd24f94
villesdev <137016082+villesdev@users.noreply.github.com>
2023-07-21 01:42:19 +0200

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import json
 from PIL import Image
 import requests
-=from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie
 
 
 def load_lottiefile(filepath: str):
