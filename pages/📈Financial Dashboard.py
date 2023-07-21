f3d366248a2a81e36c360777e23354e442aaa302
Louis Villeseche <villesechelouis@gmail.com>
2023-07-21 16:51:33 +0200

    test commit 308

--- pages/📈Financial Dashboard.py
+++ pages/📈Financial Dashboard.py
@@ -3,9 +3,6 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 
-
-# test commit 308
-
 # security visualiser
 lefttitle, righttitle, goodorbad = st.columns((2,1,1))
 with lefttitle:
